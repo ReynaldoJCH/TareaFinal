71d9f1e9bcba39a289446c8c4b3ad128dd273a88
ReynaldoJCH <reynaldo2004rg@gmail.com>
2026-05-30 16:18:36 -0600

    Creación de la clase Producto
    Se creó la clase productos

--- ProyectoFinal/ProyectoFinal/Form1.cs
+++ ProyectoFinal/ProyectoFinal/Form1.cs
@@ -66,4 +66,13 @@ namespace ProyectoFinal
             }
         }
     }
+    class Producto
+    {
+        public string Nombre { get; set; }
+        public string Precio { get; set; }
+        public string Descripcion { get; set; }
+        public string Telefono { get; set; }
+        public string Productora { get; set; }
+        public string Imagen { get; set; }
+    }
 }
